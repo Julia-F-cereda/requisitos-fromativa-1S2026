76cffe2d2477e6cac36a08f15cf77c21148d7716
julia cereda <julia.cereda@aluno.senai.br>
2026-03-12 17:31:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+from model.cadastro import enviar_dados 
+from model.cadastro import retornar_dados
+from model.cadastro import excluir_dados
+
+app = Flask(__name__)
+
+# nao sei se precisa disso
+@app.route("/index", methods=["GET"])
+@app.route("/")
+
+def pg_index():
+    return render_template("index.html")
+
+#rota para qrequisitos
+@app.route("/requisitos")
+def pg_requisitos():
+    dados = retornar_dados()
+    return render_template("requisitos.html", dados = dados)
+
+@app.route("/cadastro/post", methods=["POST"])
+def api_enviar():
+    descricao = request.form.get("descricao")
+    nivel = request.form.get("nivel")
+    valor = request.form.get("valor")
+    situacao = request.form.get("situacao")
+    enviar_dados(descricao, nivel, valor, situacao)
+    return redirect("/requisitos")
+
+@app.route("/cadastro/deletar/<cod_requisito>")
+def api_excluir_musica(cod_requisito):
+excluir_dados(cod_requisito):
+    return redirect("/requisitos")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model/cadastro.py
@@ -0,0 +1,24 @@
+from database.conexao import conectar
+
+def retornar_dados():
+    conexao, cursor = conectar()
+    cursor.execute("""SELECT descricao, nivel, valor, situacao FROM tb_requisitos""")
+    dado = cursor.fetchall()
+    conexao.close()
+    return dado
+
+def enviar_dados(descricao:str, nivel:str, valor: str, situacao:str) ->bool:
+    conexao, cursor = conectar()
+    cursor.execute("""INSERT INTO tb_requisitos (descricao, nivel, valor, situacao)
+                   VALUES (%s, %s, %s, %s)""", [descricao, nivel, valor,situacao])
+    
+    conexao.commit()
+    conexao.close()
+
+def excluir_dados(cod_requisito):
+    conexao, cursor = conectar()
+
+    cursor.execute("DELETE FROM tb_requisitos where cod_requisito= %s",[cod_requisito,])
+
+    conexao.commit()
+    conexao.close()
